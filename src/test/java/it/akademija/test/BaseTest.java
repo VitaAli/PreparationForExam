f77ee5a3a7b51840831290041b507d721fe4998f
Vitalija Ališauskienė <vitalija.alisauskiene@stud.vtmc.lt>
2023-06-11 18:08:43 +0300

    abstract page and base test classes added

---
+++ src/test/java/it/akademija/test/BaseTest.java
@@ -0,0 +1,24 @@
+package it.akademija.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+    public static WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+//        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://op.akademijait.vtmc.lt/");
+    }
+
+    @AfterEach
+    public void closeDriver() {
+        driver.quit();
+    }
+}
