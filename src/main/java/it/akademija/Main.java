0d49e7fd31cd6eaa82b3a47793fc79fe9dd8e8e7
Vitalija Ališauskienė <vitalija.alisauskiene@stud.vtmc.lt>
2023-06-11 17:35:15 +0300

    gitignore file updated

--- src/main/java/it/akademija/Main.java
+++ src/main/java/it/akademija/Main.java
@@ -1,4 +1,6 @@
-package it.akademija;public class Main {
+package it.akademija;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
